b2b8a5ca7984f70ef0565168c26eb32bf1b33661
steflef <prof.lebrun@gmail.com>
2013-01-15 16:37:16 -0500

    ng app static for now

---
+++ www/static/app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var staticApp = angular.module('staticApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
